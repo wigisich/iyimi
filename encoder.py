af6d671a9dba899c96d9f135d0b13a07cdff1df8
wigisich <battuhanfidan@gmail.com>
2023-11-18 00:31:01 +0300

    update binary_pair_encoder

--- encoder.py
+++ encoder.py
@@ -12,14 +12,17 @@ def frequencies(patterns: list[str] | set[str], data: str | list[str]) -> dict:
        freq[pattern] += data.count(pattern)
     return freq
 
-def binary_pair_encoder(data: str, itr: int):
+def binary_pair_encoder(data: str):
     data_dict = frequencies(set(data), data)
-    for _ in range(itr):
+    last = False
+    while last != data_dict:
+        last = data_dict.copy()
         most_occuring = sorted(data_dict, key=data_dict.get, reverse=True)[:2]
         new_pattern = "".join(most_occuring)
         data_dict[new_pattern] = data.count(new_pattern)
         data_dict[most_occuring[0]] -= data_dict[new_pattern]
         data_dict[most_occuring[1]] -= data_dict[new_pattern]
+
     return {k:v for k,v in data_dict.items() if v}
 
 
